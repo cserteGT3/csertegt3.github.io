c00a87e3c1b6d71c212b6b63416fdcf1643df517
cserteGT3 <csertegt@gmail.com>
2019-06-18 22:50:46 +0200

    add swim workout and csv parser

--- _drafts/processrun.jl
+++ _drafts/processrun.jl
@@ -3,15 +3,16 @@ module WorkoutProcessor
 using Dates
 
 
-export AbstractWorkoutType, RunningWorkout, GymWorkout
+export AbstractWorkoutType, RunWorkout, GymWorkout, SwimWorkout
 export Workout
+export parseworkouttype, parseline
 
 abstract type AbstractWorkoutType end
 
-struct RunningWorkout <: AbstractWorkoutType
+struct RunWorkout <: AbstractWorkoutType
 	road::String
-	distance::Float64
 	duration::Time
+	distance::Float64
 end
 
 struct GymWorkout <: AbstractWorkoutType
@@ -19,15 +20,49 @@ struct GymWorkout <: AbstractWorkoutType
 	duration::Time
 end
 
+struct SwimWorkout <: AbstractWorkoutType
+	type::String
+	duration::Time
+	distance::Float64
+end
 
 struct Workout{T<:AbstractWorkoutType}
 	date::DateTime
 	weight::Float64
-	workout::T
 	place::String
+	workout::T
 	effort::Union{Int64, Nothing}
 	notes::Union{String, Nothing}
 end
 
+const DATEFORMAT = "yyyy.mm.dd-H:M"
 
+nothinger(x) = x == "" ? nothing : x
+nothinger(x::AbstractString) = x == "" ? nothing : String(x)
+
+
+function parseworkouttype(l)
+	if l[5] == "run"
+		# run workout
+		return RunWorkout(String(l[6]), Time(l[7], DateFormat("H:M:S")), l[8])
+	elseif l[5] == "swim"
+		# swim workout
+		return SwimWorkout(String(l[6]), Time(l[7], DateFormat("H:M:S")), l[8])
+	elseif l[5] == "gym"
+		# gym workout
+		return GymWorkout(String(l[6]), Time(l[7], DateFormat("H:M:S")))
+	else
+		throw(ArgumentError(l[5]*" is not a valid workout type!"))
+		return nothing
+	end
 end
+
+
+function parseline(l, dateform)
+	time = DateTime(l[1]*"-"*l[2], dateform)
+	inot = nothinger(l[9])
+	comnot = nothinger(l[10])
+	Workout(time, l[3], String(l[4]), parseworkouttype(l), inot, comnot)
+end
+
+end # module
